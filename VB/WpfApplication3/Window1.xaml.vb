4b34d0ad8d4e585fe3eb03a1161d57e64598c847
DevExpressExampleBot <fakeEmail>
2022-12-16 22:30:42 +0400

    Source auto update [skip ci]

--- VB/WpfApplication3/Window1.xaml.vb
+++ VB/WpfApplication3/Window1.xaml.vb
@@ -1,35 +1,25 @@
-﻿Imports System
-Imports System.Collections.Generic
-Imports System.Linq
-Imports System.Text
 Imports System.Windows
 Imports System.Windows.Controls
-Imports System.Windows.Data
-Imports System.Windows.Documents
-Imports System.Windows.Input
-Imports System.Windows.Media
-Imports System.Windows.Media.Imaging
-Imports System.Windows.Navigation
-Imports System.Windows.Shapes
 Imports DevExpress.Xpf.Bars
 
 Namespace WpfApplication3
-	''' <summary>
-	''' Interaction logic for Window1.xaml
-	''' </summary>
-	Partial Public Class Window1
-		Inherits Window
 
-		Public Sub New()
-			InitializeComponent()
-		End Sub
+    ''' <summary>
+    ''' Interaction logic for Window1.xaml
+    ''' </summary>
+    Public Partial Class Window1
+        Inherits Window
 
-		Private Sub checkBtnItalic_CheckChanged(ByVal sender As Object, ByVal e As ItemClickEventArgs)
-			'...
-		End Sub
+        Public Sub New()
+            Me.InitializeComponent()
+        End Sub
 
-		Private Sub checkBtnBold_CheckChanged(ByVal sender As Object, ByVal e As DevExpress.Xpf.Bars.ItemClickEventArgs)
-			'...
-		End Sub
-	End Class
+        Private Sub checkBtnItalic_CheckChanged(ByVal sender As Object, ByVal e As ItemClickEventArgs)
+        '...
+        End Sub
+
+        Private Sub checkBtnBold_CheckChanged(ByVal sender As Object, ByVal e As ItemClickEventArgs)
+        '...
+        End Sub
+    End Class
 End Namespace
